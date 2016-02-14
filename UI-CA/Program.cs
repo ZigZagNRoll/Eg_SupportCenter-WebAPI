6a4be622540bcd5bee20bc506c2bf629d9834b97
Lukas Hanot <punk_4_life@live.be>
2016-02-14 18:23:41 +0100

    BB Versie
    Deze update wijzigt alle bestanden naar de geleverde model oplossing

--- UI-CA/Program.cs
+++ UI-CA/Program.cs
@@ -40,7 +40,7 @@ namespace SC.UI.CA
       }
       catch (Exception e)
       {
-        Console.WriteLine();
+        Console.WriteLine(e.Message);
         Console.WriteLine("Er heeft zich een onverwachte fout voorgedaan!");
         Console.WriteLine();
       }
